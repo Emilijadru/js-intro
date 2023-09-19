bf7f548b433b7c876d420a139575dc17923f8093
Emilija <emuda123@gmai.com>
2023-09-19 11:16:03 +0300

    numbers

---
+++ kintamieji/index.js
@@ -0,0 +1,9 @@
+/*
+Kintamieji
+- number
+- string9 (tekstas)
+- boolean (true/false)
+- array (masyvas/ kolekcija/ sarasas / listas)
+- object (objektas)
+- 
+*/
---
+++ kintamieji/number.js
@@ -0,0 +1,22 @@
+/* 
+Number
+- teigiamieji / neigiamieji
+- sveikieji / desimtainiai
+- normalus - nenormalus
+
+iniciavimo budas:
+- cons (default)
+- let (jei kinta info)
+- var (niekada)
+*/ 
+
+const amzius = 99;
+let pinigine = 0;
+var minusas = -7.56;
+
+const x1 = 1;
+const x2 = 2;
+const x3 = 3;
+
+console.log(amzius);
+console.log(pinigine);
