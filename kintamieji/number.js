8aca375bbd525ab5cadfa32ee8212e963c2e1c02
Emilija <emuda123@gmai.com>
2023-09-21 08:50:56 +0300

    infiniti

--- kintamieji/number.js
+++ kintamieji/number.js
@@ -14,6 +14,9 @@ const amzius = 99;
 let pinigine = 0;
 var minusas = -7.56;
 
+console.log(amzius)
+console.log(pinigine)
+
 const x1 = 1;
 const x2 = 2;
 const x3 = 3;
@@ -21,5 +24,33 @@ const x4 = 4;
 const x5 = 5;
 
 
-console.log(amzius);
-console.log(pinigine);
+const n1 = 7;
+const n2 = 5;
+
+const sum = n1 + n2;
+console.log(sum);
+
+const complex = 2 + 2 * 2;
+console.log(complex);
+
+const exp1 = 2 + 2;
+const exp2 = 2 * 2;
+const exp3 = 2 ** 2;
+ console.log(exp1, exp2, exp3);
+ 
+ const begalybe = Infinity
+ const minusBegalybe = Infinity
+
+ const x = 5 / 0;
+ console.log(x);
+
+ // not-a-number = NaN
+
+ const n = NaN;
+ console.log(n, NaN);
+
+ const liekana = 7 % 5;
+ console.log(liekana);
+
+ 
+ 
